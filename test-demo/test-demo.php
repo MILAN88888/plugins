0ad2e20cfa0f6b4450836f1d31b270e36cd96d9f
MILAN88888 <chaudharymilan996@gmail.com>
2023-01-19 14:59:44 +0545

    for practise

--- test-demo/test-demo.php
+++ test-demo/test-demo.php
@@ -31,6 +31,44 @@ function wporg_register_taxonomy_course() {
     );
     register_taxonomy( 'course', [ 'post' ], $args );
 }
+
+function wporg_custom_post_type() {
+	register_post_type('wporg_product',
+		array(
+			'labels'      => array(
+				'name'          => __('Products', 'textdomain'),
+				'singular_name' => __('Product', 'textdomain'),
+			),
+				'public'      => true,
+				'has_archive' => true,
+		)
+	);
+}
+function wporg_custom_box_html( $post ) {
+	?>
+	<form>
+        <label class="form-label">Name</label>
+        <input class="form-control" type="text" name="name" id="name" />
+        <label class="form-label">Address</label>
+        <input class="form-control" type="text" name="address" />
+    </form>
+	
+	<?php
+}
+
+function wporg_add_custom_box() {
+	$screens = [ 'post', 'wporg_cpt' ];
+	foreach ( $screens as $screen ) {
+		add_meta_box(
+			'wporg_box_id',                 // Unique ID
+			'Custom Meta Box Title',      // Box title
+			'wporg_custom_box_html',  // Content callback, must be of type callable
+			$screen                            // Post type
+		);
+	}
+}
+
 add_action('init', 'wporg_custom_post_type');
 add_action( 'init', 'wporg_register_taxonomy_course' );
+add_action( 'add_meta_boxes', 'wporg_add_custom_box' );
 ?>
